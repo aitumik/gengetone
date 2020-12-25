4d74f66732a84708db7fad65e40cec1d075237a2
aitumik <nathanpykimutai@gmail.com>
2020-12-25 13:02:14 +0300

    trying to work a little faster because there is no time left this year for crap

--- app/src/main/java/com/aitumik/nathanplayer/models/Song.kt
+++ app/src/main/java/com/aitumik/nathanplayer/models/Song.kt
@@ -1,7 +1,10 @@
 package com.aitumik.nathanplayer.models
 
 import android.database.Cursor
+import android.support.v4.media.MediaBrowserCompat
+import android.support.v4.media.MediaDescriptionCompat
 import com.google.gson.Gson
+import java.io.File
 
 data class Song (
         val id: Long = -1,
@@ -32,7 +35,44 @@ data class Song (
 //                                        path = getSongUri(cursor.getLong(0)).toString()
                                 )
                         }
+
+                        fun createFromPlaylistCursor(cursor: Cursor): Song {
+                                return Song(
+                                        id = cursor.getLong(0),
+                                        title = cursor.getString(1),
+                                        artist = cursor.getString(2),
+                                        album = cursor.getString(3),
+                                        duration = cursor.getInt(4),
+                                        trackNumber = cursor.getInt(5),
+                                        artistId = cursor.getLong(6),
+                                        albumId = cursor.getLong(7),
+                                        playListId = cursor.getLong(8),
+                                        path = getSongUri(cursor.getLong(0)).toString()
+                                )
+                        }
+
+                        fun createFromFolderCursor(cursor: Cursor): Song {
+                                return Song(
+                                        id = cursor.getLong(0),
+                                        title = cursor.getString(1),
+                                        artist = cursor.getString(2),
+                                        album = cursor.getString(3),
+                                        duration = cursor.getInt(4),
+                                        trackNumber = cursor.getInt(5),
+                                        artistId = cursor.getLong(6),
+                                        albumId = cursor.getLong(7),
+                                        path = File(cursor.getString(8)).parent!!
+                                )
+                        }
+
+                        fun createFromAudioFile(audioFile: AudioFile): Song {
+
+                        }
                 }
+
+        fun toMediaItem() {
+                TODO("finish creatng the song model and make sure it does not show the error")
+        }
 
         override fun toString(): String {
                 return Gson().toString()
