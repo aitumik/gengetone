92eecaf14d916aa3493100447bdfd6e951ee2509
aitumik <nathanpykimutai@gmail.com>
2020-12-26 15:16:26 +0300

    created an autoclear binding

--- app/src/main/java/com/aitumik/nathanplayer/utils/AutoClearBinding.kt
+++ app/src/main/java/com/aitumik/nathanplayer/utils/AutoClearBinding.kt
@@ -1,4 +1,26 @@
 package com.aitumik.nathanplayer.utils
 
-class AutoClearBinding {
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.LifecycleObserver
+import kotlin.properties.ReadWriteProperty
+import kotlin.reflect.KProperty
+
+class AutoClearBinding<T: Any>(fragment: Fragment): ReadWriteProperty<Fragment,T> {
+    private var value : T? = null
+
+    init {
+        fragment.lifecycle.addObserver(object : LifecycleObserver {
+            fun onDestroy() {
+                value = null
+            }
+        })
+    }
+
+    override fun setValue(thisRef: Fragment, property: KProperty<*>, value: T) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getValue(thisRef: Fragment, property: KProperty<*>): T {
+        TODO("Not yet implemented")
+    }
 }
